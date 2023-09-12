1ba9d42c3a3a389d336d087ca24bfbe4c3123fc5
Герман <108299345+german-spb@users.noreply.github.com>
2023-09-12 23:02:36 +0300

    Update task.js

--- task.js
+++ task.js
@@ -1,12 +1,12 @@
 const a = document.querySelectorAll('.menu__link')
 for (let i of a){
     i.onclick = () => {
-        if (i.nextElementSibling != null){
+        if (i.nextElementSibling != null){  
                 if(i.nextElementSibling.classList.contains('menu_sub')){
-                i.nextElementSibling.classList.toggle('menu_active')    
-                console.log(i.nextElementSibling.classList)
+                    i.nextElementSibling.classList.toggle('menu_active')
+                    
                 }
+                return false;
             }
-        return false;     
-        }
+    }
 }   
